5aea269960f2712fa0dc4d65027cdec300ab1419
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-03-27 12:51:26 -0700

    Fix slow miri test_equivalence_generic

--- src/reproducibility.rs
+++ src/reproducibility.rs
@@ -332,7 +332,11 @@ fn test_equivalence_generic<R: Reproducibility>() {
     let seed = [0u8; DEFAULT_SEED_SIZE];
     let mut prng1 = TripleMixPrng::<R>::from(&seed);
     let mut prng2 = TripleMixPrng::<R>::from(&seed);
-    for length in [1, 2, 4, 8, 16, 32, 64, 1024] {
+    #[cfg(not(miri))]
+    const LENGTHS: &[usize] = &[1, 2, 4, 8, 16, 32, 64, 1024];
+    #[cfg(miri)]
+    const LENGTHS: &[usize] = &[4];
+    for length in LENGTHS {
         for misalignment in 0..size_of::<u64>() {
             // Force buffer edges to be aligned on 64 bits, so that written portion will be misaligned
             let mut buf1 = vec![0u64; (length + size_of::<u64>()) / size_of::<u64>() + 1];
