34033a2c7e43402d3a85994838e12b05999fde75
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-03-27 18:28:44 -0700

    Move constants into `mix()` that are only used there

--- src/generate.rs
+++ src/generate.rs
@@ -37,35 +37,6 @@ impl<R: Reproducibility> TripleMixSimdCore<R> {
         u64::MAX - Self::MULTIPLIER_COMPLEMENT_3 + 1,
     ]);
 
-    const PCG_MULT_LO_0: u64 = 0x1FC6_5DA5;
-    const PCG_MULT_HI_0: u64 = 0x2360_ED05;
-    const PCG_MULT_LO_1: u64 = 0x4C95_7F2D;
-    const PCG_MULT_HI_1: u64 = 0x8F1C_5E95;
-    const PCG_MULT_LO_2: u64 = 0xE5A7_4D29;
-    const PCG_MULT_HI_2: u64 = 0xA3E7_9B3D;
-    const PCG_MULT_LO_3: u64 = 0x9B3C_D8F1;
-    const PCG_MULT_HI_3: u64 = 0x2360_ED05;
-
-    const PCG_MULT_LO: Simd64 = Simd64::from_array([
-        Self::PCG_MULT_LO_0,
-        Self::PCG_MULT_LO_1,
-        Self::PCG_MULT_LO_2,
-        Self::PCG_MULT_LO_3,
-    ]);
-    const PCG_MULT_HI: Simd64 = Simd64::from_array([
-        Self::PCG_MULT_HI_0,
-        Self::PCG_MULT_HI_1,
-        Self::PCG_MULT_HI_2,
-        Self::PCG_MULT_HI_3,
-    ]);
-
-    pub(crate) const PCG_MULTIPLIERS: Simd64 = Simd64::from_array([
-        (Self::PCG_MULT_HI_0 << 32) | Self::PCG_MULT_LO_0,
-        (Self::PCG_MULT_HI_1 << 32) | Self::PCG_MULT_LO_1,
-        (Self::PCG_MULT_HI_2 << 32) | Self::PCG_MULT_LO_2,
-        (Self::PCG_MULT_HI_3 << 32) | Self::PCG_MULT_LO_3,
-    ]);
-
     /// Multiplies two vectors. Requires that all elements of b be less than 2^32. Returns (low, hi)
     /// halves of result.
     #[inline(always)]
@@ -152,11 +123,41 @@ impl<R: Reproducibility> TripleMixSimdCore<R> {
             0x94d049bb133111eb,
         ]);
 
+            const PCG_MULT_LO_0: u64 = 0x1FC6_5DA5;
+    const PCG_MULT_HI_0: u64 = 0x2360_ED05;
+    const PCG_MULT_LO_1: u64 = 0x4C95_7F2D;
+    const PCG_MULT_HI_1: u64 = 0x8F1C_5E95;
+    const PCG_MULT_LO_2: u64 = 0xE5A7_4D29;
+    const PCG_MULT_HI_2: u64 = 0xA3E7_9B3D;
+    const PCG_MULT_LO_3: u64 = 0x9B3C_D8F1;
+    const PCG_MULT_HI_3: u64 = 0x2360_ED05;
+
+    const PCG_MULT_LO: Simd64 = Simd64::from_array([
+        PCG_MULT_LO_0,
+        PCG_MULT_LO_1,
+        PCG_MULT_LO_2,
+        PCG_MULT_LO_3,
+    ]);
+    const PCG_MULT_HI: Simd64 = Simd64::from_array([
+        PCG_MULT_HI_0,
+        PCG_MULT_HI_1,
+        PCG_MULT_HI_2,
+        PCG_MULT_HI_3,
+    ]);
+
+    pub(crate) const PCG_MULTIPLIERS: Simd64 = Simd64::from_array([
+        (PCG_MULT_HI_0 << 32) | PCG_MULT_LO_0,
+        (PCG_MULT_HI_1 << 32) | PCG_MULT_LO_1,
+        (PCG_MULT_HI_2 << 32) | PCG_MULT_LO_2,
+        (PCG_MULT_HI_3 << 32) | PCG_MULT_LO_3,
+    ]);
+
+
         for block in blocks {
             // Kick off the highest latency operations (multipliers) early
             // a_low * b (where b is pcg_mult)
-            let (pcg_p1_lo, pcg_p1_hi) = Self::simd_mulsmall(pcg_state_lo, Self::PCG_MULT_LO);
-            let (pcg_p2_lo, pcg_p2_hi) = Self::simd_mulsmall(pcg_state_lo, Self::PCG_MULT_HI);
+            let (pcg_p1_lo, pcg_p1_hi) = Self::simd_mulsmall(pcg_state_lo, PCG_MULT_LO);
+            let (pcg_p2_lo, pcg_p2_hi) = Self::simd_mulsmall(pcg_state_lo, PCG_MULT_HI);
             // a_high * b = a_high * b_lo + a_high * b_hi * 2^32
             let pcg_a_high_b = simd_wrapping_mul(pcg_state_hi, Self::PCG_MULTIPLIERS);
 
