dcdc7a7c7ceafae718250bdaf356a2706615a876
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-04-05 15:06:19 -0700

    Fix: llvm-mca is also used in endless_bytes_threadrng.rs

--- examples/endless_bytes_threadrng.rs
+++ examples/endless_bytes_threadrng.rs
@@ -4,16 +4,17 @@ use rand::rngs::ThreadRng;
 use rand::rng;
 use rand_core::Rng;
 use std::io::{Write, stdout};
-use llvm_mca::{llvm_mca_begin, llvm_mca_end};
 
 fn main() {
     let mut prng: ThreadRng = rng();
     let mut stdout = stdout().lock();
     let mut buffer = [0u8; 1 << 16];
     loop {
-        llvm_mca_begin!("fill_bytes");
+        #[cfg(feature = "llvm-mca")]
+        llvm_mca::llvm_mca_begin!("fill_bytes");
         prng.fill_bytes(&mut buffer);
-        llvm_mca_end!("fill_bytes");
+        #[cfg(feature = "llvm-mca")]
+        llvm_mca::llvm_mca_end!("fill_bytes");
         if let Err(e) = stdout.write_all(&buffer) {
             eprintln!("Error writing to stdout: {}", e);
             return;
