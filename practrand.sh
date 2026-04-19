618ff8d67919ae63b5335b1d243a1249dab8c1b1
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2026-04-19 12:30:19 -0700

    Tweak: zero-pad output filenames so they're in order

--- practrand.sh
+++ practrand.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-mkdir -p target
 (
   # Do shortest runs first for quick feedback
   for _ in {1..4096}; do echo "r 1G"; done
@@ -11,8 +10,8 @@ mkdir -p target
   for _ in {1..32}; do echo "r 1T"; done
   for _ in {1..128}; do echo "r 128G"; done
   for _ in {1..1024}; do echo "r 16G"; done
-) | parallel -j 5 --colsep ' ' --lb ' \
-  ./target/release/examples/endless_bytes {1} 2> >(tee target/practrand_{#}.txt) \
+) | parallel --plus -j 5 --colsep ' ' --lb ' \
+  ./target/release/examples/endless_bytes {1} 2> >(tee target/practrand_{000#}.txt) \
   | /mnt/c/Users/cryoc/Downloads/PractRand_0.96/PractRand/RNG_test \
     stdin -multithreaded -tlmax {2} -tlshow 6T -tlshow 10T -tlshow 12T -tlshow 14T -tlshow 18T -tlshow 20T -tlshow 22T -tlshow 24T -tlshow 26T -tlshow 28T -tlshow 30T 2>&1 \
-  | tee -a target/practrand_{#}.txt'
+  | tee -a target/practrand_{000#}.txt'
